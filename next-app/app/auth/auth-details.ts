977458bcc13542ef43cb24b60f667fe31c22acdf
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-11-09 11:07:23 -0800

    add: testing api gateway

--- next-app/app/auth/auth-details.ts
+++ next-app/app/auth/auth-details.ts
@@ -5,6 +5,7 @@ export interface CognitoSettings {
   domain: string;
   response_type: string;
   scopes: string;
+  gateway_url: string
 }
 
 const SCOPES = "email openid";
@@ -22,6 +23,7 @@ function normalizeConfig(data: Record<string, string>): CognitoSettings {
     domain: data.NEXT_PUBLIC_COGNITO_DOMAIN || data.COGNITO_DOMAIN!,
     response_type: RESPONSE_TYPE,
     scopes: SCOPES,
+    gateway_url: data.GATEWAY_URL || ""
   };
 }
 
@@ -30,24 +32,36 @@ export async function getAuthVars(): Promise<CognitoSettings> {
 
   let data: Record<string, string>;
 
-  if (process.env.NEXT_PUBLIC_COGNITO_ENDPOINT) {
+  const NEXT_PUBLIC_COGNITO_CLIENT_ID = process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || ""
+  const NEXT_PUBLIC_COGNITO_ENDPOINT = process.env.NEXT_PUBLIC_COGNITO_ENDPOINT || ""
+  const NEXT_PUBLIC_COGNITO_DOMAIN = process.env.NEXT_PUBLIC_COGNITO_DOMAIN || ""
+  const NEXT_PUBLIC_COGNITO_REDIRECT_URI = process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI || ""
+
+  const hasLocalEnv = 
+    NEXT_PUBLIC_COGNITO_CLIENT_ID &&
+    NEXT_PUBLIC_COGNITO_ENDPOINT &&
+    NEXT_PUBLIC_COGNITO_DOMAIN &&
+    NEXT_PUBLIC_COGNITO_REDIRECT_URI;
+
+  if (hasLocalEnv) {
     // Local dev
     data = {
-      NEXT_PUBLIC_COGNITO_ENDPOINT: process.env.NEXT_PUBLIC_COGNITO_ENDPOINT!,
-      NEXT_PUBLIC_COGNITO_CLIENT_ID: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!,
-      NEXT_PUBLIC_COGNITO_REDIRECT_URI:
-        process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI!,
-      NEXT_PUBLIC_COGNITO_DOMAIN: process.env.NEXT_PUBLIC_COGNITO_DOMAIN!,
+      NEXT_PUBLIC_COGNITO_ENDPOINT,
+      NEXT_PUBLIC_COGNITO_CLIENT_ID,
+      NEXT_PUBLIC_COGNITO_REDIRECT_URI,
+      NEXT_PUBLIC_COGNITO_DOMAIN,
     };
     console.log("Using local NEXT_PUBLIC env for Cognito config:", data);
   } else {
     // Production: fetch from Lambda
-    if (!process.env.NEXT_PUBLIC_COGNITO_CONFIG_URI) {
-      throw new Error("NEXT_PUBLIC_COGNITO_CONFIG_URI is not set");
-    }
-    const res = await fetch(process.env.NEXT_PUBLIC_COGNITO_CONFIG_URI);
+    const GATEWAY_URL = "https://jx7siawh2a.execute-api.us-east-1.amazonaws.com/prod" // PLACEHOLDER_URL for sed
+    // if (GATEWAY_URL === "PLACEHOLDER_URL") {
+    //   throw new Error("GATEWAY_URL was not replaced during build - check your sed script");
+    // }
+    const res = await fetch(`${GATEWAY_URL}/api/cognito`);
     if (!res.ok) throw new Error("Failed to fetch Cognito config from Lambda");
     data = await res.json();
+    data.GATEWAY_URL = GATEWAY_URL
     console.log("Fetched Cognito config from Lambda:", data);
   }
 
