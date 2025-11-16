f78bbe31737a25625eb8e62544ac3b41a284e475
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-11-16 14:50:57 -0800

    update: ffmpeg zip name

--- infra/aws/ffmpeg_lambda_layer.tf
+++ infra/aws/ffmpeg_lambda_layer.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_layer_version" "ffmpeg_layer" {
-  filename         = "${path.module}/lambda/ffmpeg/ffmpeg.zip"
+  filename         = "${path.module}/lambda/ffmpeg/ffmpeg-layer.zip"
   layer_name       = "ffmpeg"
   description      = "FFMPEG layer for lambda video processing"
   license_info     = "MIT"
-  source_code_hash = filebase64sha256("${path.module}/lambda/ffmpeg/ffmpeg.zip")
+  source_code_hash = filebase64sha256("${path.module}/lambda/ffmpeg/ffmpeg-layer.zip")
 
   compatible_runtimes = ["python3.12"]
 
