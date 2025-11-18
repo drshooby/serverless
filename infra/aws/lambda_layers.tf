9bba63368371e30530f3aadb8c85724c92f3f700
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-11-17 21:17:45 -0800

    update: create unified `lambda_layers.tf` file

--- infra/aws/lambda_layers.tf
+++ infra/aws/lambda_layers.tf
@@ -9,3 +9,15 @@ resource "aws_lambda_layer_version" "ffmpeg_layer" {
 
   compatible_architectures = ["x86_64"]
 }
+
+resource "aws_lambda_layer_version" "psycopg_layer" {
+  filename         = "${path.module}/lambda/psycopg/psycopg-layer.zip"
+  layer_name       = "psycopg"
+  description      = "Psycopg layer for lambda db usage"
+  license_info     = "MIT"
+  source_code_hash = filebase64sha256("${path.module}/lambda/psycopg/psycopg-layer.zip")
+
+  compatible_runtimes = ["python3.12"]
+
+  compatible_architectures = ["x86_64"]
+}
